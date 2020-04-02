bc4c09b034061c5be4b9dbf03af3c566d25e506a
Jack Cooper <jackcooper1245@gmail.com>
2020-04-02 10:10:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index_number)
-if board[index_number] != " "
-  return true
-else board[index_number] == " " || board[index_number] == "" || board[index_number] == nil
+if board[index_number] == " " || board[index_number] == "" || board[index_number] == nil
   return false
+else 
+  return true
 end
 end
